2fb81b78e6249ddc4a2939eb845c2e8b4893271a
ZhangShiyang <shiyang.zhang@kuwo.cn>
2017-04-14 11:27:58 +0800

    compile arch-arm arch-x86

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,55 +2,55 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := yuv_static
-LOCAL_SRC_FILES := libyuv/lib/libyuv_static.a
+LOCAL_SRC_FILES := libyuv/lib/$(TARGET_ARCH_ABI)/libyuv_static.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := x264
-LOCAL_SRC_FILES := libx264/lib/libx264.a
+LOCAL_SRC_FILES := libx264/lib/$(TARGET_ARCH_ABI)/libx264.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := faac
-LOCAL_SRC_FILES := libfaac/lib/libfaac.a
+LOCAL_SRC_FILES := libfaac/lib/$(TARGET_ARCH_ABI)/libfaac.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := avutil
-LOCAL_SRC_FILES := ffmpeg/lib/libavutil.a
+LOCAL_SRC_FILES := ffmpeg/lib/$(TARGET_ARCH_ABI)/libavutil.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := swscale
-LOCAL_SRC_FILES := ffmpeg/lib/libswscale.a
+LOCAL_SRC_FILES := ffmpeg/lib/$(TARGET_ARCH_ABI)/libswscale.a
 LOCAL_LDLIBS := -lm
 LOCAL_STATIC_LIBRARIES := avutil
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := swresample
-LOCAL_SRC_FILES := ffmpeg/lib/libswresample.a
+LOCAL_SRC_FILES := ffmpeg/lib/$(TARGET_ARCH_ABI)/libswresample.a
 LOCAL_LDLIBS := -lm
 LOCAL_STATIC_LIBRARIES := avutil
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := avcodec
-LOCAL_SRC_FILES := ffmpeg/lib/libavcodec.a
+LOCAL_SRC_FILES := ffmpeg/lib/$(TARGET_ARCH_ABI)/libavcodec.a
 LOCAL_LDLIBS := -lm -lz
 LOCAL_STATIC_LIBRARIES := avutil swresample x264 faac
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := avformat
-LOCAL_SRC_FILES := ffmpeg/lib/libavformat.a
+LOCAL_SRC_FILES := ffmpeg/lib/$(TARGET_ARCH_ABI)/libavformat.a
 LOCAL_LDLIBS := -lm -lz
 LOCAL_STATIC_LIBRARIES := avutil swresample avcodec
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := avfilter
-LOCAL_SRC_FILES := ffmpeg/lib/libavfilter.a
+LOCAL_SRC_FILES := ffmpeg/lib/$(TARGET_ARCH_ABI)/libavfilter.a
 LOCAL_LDLIBS := -lm -lz
 LOCAL_STATIC_LIBRARIES := avutil swresample avcodec swscale avformat
 include $(PREBUILT_STATIC_LIBRARY)
