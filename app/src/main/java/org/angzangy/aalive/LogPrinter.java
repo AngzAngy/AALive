1bf75b51d83d29d0e87586da1e9db8d4b3f70acc
ZhangShiyang <shiyang.zhang@kuwo.cn>
2020-10-16 17:24:23 +0800

    log print error

--- app/src/main/java/org/angzangy/aalive/LogPrinter.java
+++ app/src/main/java/org/angzangy/aalive/LogPrinter.java
@@ -7,4 +7,7 @@ public class LogPrinter {
     public static void d(String message){
         Log.d(TAG, message);
     }
+    public static void e(String message){
+        Log.e(TAG, message);
+    }
 }
