167a491da53b6afc3bc3cdd2ca9fd237935b9c88
ZhangShiyang <shiyang.zhang@kuwo.cn>
2020-10-21 11:14:07 +0800

    reflection filter

--- app/src/main/java/org/angzangy/aalive/gles/SurfaceTextureRenderer.java
+++ app/src/main/java/org/angzangy/aalive/gles/SurfaceTextureRenderer.java
@@ -14,6 +14,41 @@ public class SurfaceTextureRenderer extends Texture2DRenderer{
             "  gl_FragColor = texture2D(sTexture, vTextureCoord);\n" +
             "}\n";
 
+    public final  static String OES_ReflectionFragmentShader =
+            "#extension GL_OES_EGL_image_external : require\n" +
+                    "precision mediump float;\n" +
+                    "varying vec2 vTextureCoord;\n" +
+                    "uniform samplerExternalOES sTexture;\n" +
+                    "void main(void) {\n" +
+                        "vec2 normCoord = vTextureCoord;\n"+
+                        "float u,v;\n"+
+                        "if(normCoord.x < 0.5 && normCoord.y < 0.5){\n"+
+                            "normCoord = vTextureCoord * 2.0;\n"+
+                            "u = 0.4246; v=0.5753;\n"+
+                        "}else if(normCoord.x > 0.5 && normCoord.y < 0.5){\n"+
+                            "normCoord.x = (vTextureCoord.x -0.5) * 2.0 ;\n"+
+                            "normCoord.y = vTextureCoord.y  * 2.0 ;\n"+
+                            "u = 0.2870; v = 0.3678;\n"+
+                        "}else if(normCoord.x < 0.5 && normCoord.y > 0.5){\n"+
+                            "normCoord.x = vTextureCoord.x * 2.0 ;\n"+
+                            "normCoord.y = (vTextureCoord.y - 0.5) * 2.0 ;\n"+
+                            "u = 0.5671; v = 0.8282;\n"+
+                        "}else {\n"+
+                            "normCoord = (vTextureCoord - 0.5) * 2.0;\n"+
+                            "u = 0.5845; v = 0.4362;\n"+
+                        "}\n"+
+                        "vec4 color = texture2D(sTexture, normCoord);\n"+
+                        "float gray = 0.3*color.r + 0.59*color.g + 0.11*color.b;\n"+
+                        "if(vTextureCoord.x < 0.5 && vTextureCoord.y > 0.5){\n"+
+                        "}else {\n"+
+                            "v -= 0.5; u -= 0.5;\n"+
+                            "color.r = gray + 1.4075 * v;\n"+
+                            "color.g = gray - 0.3455 * u - 0.7169* v;\n"+
+                            "color.b = gray + 1.779*u;\n"+
+                        "}\n"+
+                        "gl_FragColor = color;\n"+
+                    "}\n";
+
     public SurfaceTextureRenderer() {
         super();
     }
